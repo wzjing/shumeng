e03f3b4d895cdd32c7e1378ce952ef590dfa89ea
wangzijing <wangzijing@miliantech.com>
2019-11-01 19:37:36 +0800

    update project build files

--- app/src/main/java/com/github/gavinme/smid/MainActivity.java
+++ app/src/main/java/com/github/gavinme/smid/MainActivity.java
@@ -3,7 +3,10 @@ package com.github.gavinme.smid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import cn.shuzilm.core.Main;
 
 public class MainActivity extends Activity {
@@ -14,7 +17,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_smid = findViewById(R.id.tv_smid);
+    }
+
+    public void generate(View v) {
         tv_smid.setText(Main.getQueryID(this));
+        Toast.makeText(this, "generated", Toast.LENGTH_SHORT).show();
     }
 
 }
